b7d5b4ae5794a00145f945e28c58a228bddd5791
jrathore85 <rathore85@gmail.com>
2020-11-06 14:25:09 +0530

    first build

---
+++ precache-manifest.fa7757b16ad8de00cea734778cf7c669.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "92dc55892f380eac7661",
+    "url": "/static/css/main.b9c7507d.chunk.css"
+  },
+  {
+    "revision": "92dc55892f380eac7661",
+    "url": "/static/js/main.92dc5589.chunk.js"
+  },
+  {
+    "revision": "8fc4b3847f00ae298cb4",
+    "url": "/static/js/1.8fc4b384.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "acdc57b66603d4ec08ed2812026b0d07",
+    "url": "/static/media/excelmock3.acdc57b6.jpg"
+  },
+  {
+    "revision": "ba7d3dbd3950b316cdf832d678099a75",
+    "url": "/static/media/Bold_logo_3.ba7d3dbd.png"
+  },
+  {
+    "revision": "1eff4a5e9a54d84604eec77832e33178",
+    "url": "/static/media/Oreneta_logo.1eff4a5e.png"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "1b6bfbaf82ad1152873688e57c0f5c48",
+    "url": "/index.html"
+  }
+];
